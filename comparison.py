b963e8a9294c89476ad21b1a9e5f7ca1564d9b7b
Oskar Harber <oskar.harber@outlook.com>
2026-02-11 14:48:18 +0000

    ke fixed
    Refactor position and force comparison functions to handle fewer input files, and update plotting functions accordingly.

--- comparison.py
+++ comparison.py
@@ -3,18 +3,15 @@ import matplotlib.pyplot as plt
 
 
 def position(sets):
-    r1,r2,r3,r4,r5 = sets
+    r1,r2 = sets
     data_r1 = np.load(r1)
     data_r2 = np.load(r2)
-    data_r3 = np.load(r3)
-    data_r4 = np.load(r4)
-    data_r5 = np.load(r5)
+
+
     p_hist_r1 = data_r1['p_hist']
     p_hist_r2 = data_r2['p_hist']
-    p_hist_r3 = data_r3['p_hist']
-    p_hist_r4 = data_r4['p_hist']
-    p_hist_r5 = data_r5['p_hist']
-    
+
+
     fall_factor_rk4 = data_r1['fall_factor']
     time = data_r1['time']
     t = p_hist_r1.shape[0]
@@ -24,15 +21,13 @@ def position(sets):
 
     y = np.array(p_hist_r1)[:, -1, 1]
     y2 = np.array(p_hist_r2)[:, -1, 1]
-    y3 = np.array(p_hist_r3)[:, -1, 1]
-    y4 = np.array(p_hist_r4)[:, -1, 1]
-    y5 = np.array(p_hist_r5)[:, -1, 1]
+
+
     
-    plt.plot(x, y, label='year 1 ')
-    plt.plot(x, y2, label='year 2')
-    plt.plot(x, y3, label='year 3')
-    plt.plot(x, y4, label='year 4')
-    plt.plot(x, y5, label='year 5')
+    plt.plot(x, y, label='Implicit - GL2 ',alpha = 0.8, c = "r")
+
+ 
+
     plt.axhline(0, color="r", linestyle="--")
     plt.xlabel('Time (s)')
     plt.ylabel('Climber Y Position (m)')
@@ -43,20 +38,18 @@ def position(sets):
 
 def plot_force_comparison(sets):
     """Load force histories from two files and plot their average rope force on the same graph."""
-    r1,r2,r3,r4,r5 = sets
+    r1,r2= sets
     data_r1 = np.load(r1)
     data_r2 = np.load(r2)
-    data_r3 = np.load(r3)
-    data_r4 = np.load(r4)
-    data_r5 = np.load(r5)
+
+
 
     
     f_hist_rk1 = data_r1['f_hist']
     f_hist_rk2 = data_r2['f_hist']
-    f_hist_rk3 = data_r3['f_hist']
-    f_hist_rk4 = data_r4['f_hist']
-    f_hist_rk5 = data_r5['f_hist']
-    data = [f_hist_rk1,f_hist_rk2,f_hist_rk3,f_hist_rk4,f_hist_rk5]
+
+
+    data = [f_hist_rk1,f_hist_rk2]
     fall_factor_rk4 = data_r1['fall_factor']
 
     plotting = []
@@ -83,11 +76,10 @@ def plot_force_comparison(sets):
     x = np.arange(t) * dt
 
     plt.figure(figsize=(10, 6))
-    plt.plot(x, plotting[0], label='1 year', alpha = 0.8, c = "r")
-    plt.plot(x, plotting[1], label='2 yeaer', alpha = 0.8, c = "hotpink")
-    plt.plot(x, plotting[2], label='3 yeaer', alpha = 0.8, c = "purple")
-    plt.plot(x, plotting[3], label='4 yeaer', alpha = 0.8, c = "indigo")
-    plt.plot(x, plotting[4], label='5 yeaer', alpha = 0.8, c = "b")
+    plt.plot(x, plotting[0], label='Implicit - GL2', alpha = 0.8, c = "r")
+
+
+
 
     plt.xlabel('Time (s)')
     plt.ylabel('Rope Force (kN)')
@@ -97,19 +89,37 @@ def plot_force_comparison(sets):
     plt.show()
 
 
-def plot_kinetic_energy_comparison(rk4_file, implicit_file, masses_rk4, masses_impl):
+def plot_kinetic_energy_comparison(sets):
     """
     Plot the total kinetic energy for both RK4 and implicit methods.
     - rk4_file, implicit_file: .npz files with v_hist
     - masses_rk4, masses_impl: 1D arrays of masses for each method (shape: [n_masses])
     """
-    data_rk4 = np.load(rk4_file)
-    data_impl = np.load(implicit_file)
-    v_hist_rk4 = data_rk4['v_hist']
-    v_hist_impl = data_impl['v_hist']
-    fall_factor_rk4 = data_rk4['fall_factor']
-    masses_rk4 = data_rk4['masses']
-    masses_impl = data_impl['masses']
+    r1,r2,r3,r4,r5 = sets
+    data_r1 = np.load(r1)
+    data_r2 = np.load(r2)
+    data_r3 = np.load(r3)
+    data_r4 = np.load(r4)
+    data_r5 = np.load(r5)
+    v_hist_rk1 = data_r1['v_hist']
+    v_hist_rk2 = data_r2['v_hist']
+    v_hist_rk3 = data_r3['v_hist']
+    v_hist_rk4 = data_r4['v_hist']
+    v_hist_rk5 = data_r5['v_hist']
+
+    p_hist_r1 = data_r1['p_hist']
+    p_hist_r2 = data_r2['p_hist']
+    p_hist_r2 = data_r2['p_hist']
+    p_hist_r2 = data_r2['p_hist']
+    p_hist_r2 = data_r2['p_hist']
+    
+    
+    fall_factor_rk4 = data_r1['fall_factor']
+    masses_rk4 = data_r1['masses']
+    masses_impl = data_r2['masses']
+    masses_3 = data_r3['masses']
+    masses_4 = data_r4['masses']
+    masses_5 = data_r5['masses']
 
 
     def total_ke(v_hist, masses):
@@ -120,18 +130,24 @@ def plot_kinetic_energy_comparison(rk4_file, implicit_file, masses_rk4, masses_i
             ke_total.append(np.sum(ke))
         return np.array(ke_total)
 
-    ke_rk4 = total_ke(v_hist_rk4, masses_rk4)
-    ke_impl = total_ke(v_hist_impl, masses_impl)
+    ke_rk4 = total_ke(v_hist_rk1, masses_rk4)
+    ke_impl = total_ke(v_hist_rk2, masses_impl)
+    ke3 = total_ke(v_hist_rk3, masses_3)
+    ke4 =total_ke(v_hist_rk4, masses_4)
+    ke5=total_ke(v_hist_rk5, masses_5)
     t_rk4 = np.arange(len(ke_rk4))
     t_impl = np.arange(len(ke_impl))
-    time = data_rk4['time']
+    time = data_r1['time']
     t = ke_rk4.shape[0]
 
     dt = time / (t - 1)
     x = np.arange(t) * dt
     plt.figure(figsize=(10, 6))
-    plt.plot(x, ke_rk4, label='RK4 Method')
-    plt.plot(x, ke_impl, label='Implicit Method')
+    plt.plot(x, ke_rk4, label='year 1 ', c='r')
+    plt.plot(x, ke_impl, label='year 2 ', c='hotpink')
+    plt.plot(x, ke3, label = 'year3', c = 'purple')
+    plt.plot(x, ke3, label = 'year4', c = 'indigo')
+    plt.plot(x, ke3, label = 'year5', c = 'b')
     plt.xlabel('Time (s)')
     plt.ylabel('Total Kinetic Energy (J)')
     plt.title(f'Total Kinetic Energy Comparison : fall factor {fall_factor_rk4}')
@@ -139,55 +155,34 @@ def plot_kinetic_energy_comparison(rk4_file, implicit_file, masses_rk4, masses_i
     plt.grid(True, alpha=0.3)
     plt.show()
 
-def tension(rk4_file, implicit_file):
-    data_rk4 = np.load(rk4_file)
-    data_impl = np.load(implicit_file)
-    f_hist_rk4 = data_rk4['f_hist']
-    f_hist_impl = data_impl['f_hist']
-    fall_factor_rk4 = data_rk4['fall_factor']
-    tensions_rk4 = []
-    for forces in f_hist_rk4:
-        tensions_rk4.append(forces[-1])
+def climber_jerk(sets):
+    r1,r2 = sets
+    data_r1 = np.load(r1)
+    data_r2 = np.load(r2)
     
-    tensions_impl = []
-    for forces in f_hist_impl:
-        tensions_impl.append(forces[-1])
+    v_hist_rk1 = data_r1['v_hist']
+    v_hist_rk2 = data_r2['v_hist']
 
-    t_rk4 = np.arange(len(tensions_rk4))
-    t_impl = np.arange(len(tensions_impl))
+    p_hist_r1 = data_r1['p_hist']
+    p_hist_r2 = data_r2['p_hist']
+    f_hist_rk1 = data_r1['f_hist']
+    f_hist_rk2 = data_r2['f_hist']
+    
+    
+    fall_factor_rk4 = data_r1['fall_factor']
+    masses_rk4 = data_r1['masses']
+    masses_impl = data_r2['masses']
+    
 
-    time = data_rk4['time']
-    t = f_hist_rk4.shape[0]
-
-    dt = time / (t - 1)
-    x = np.arange(t) * dt
-    plt.figure(figsize=(10, 6))
-    plt.plot(x, tensions_rk4, label='RK4 Method', alpha = 0.8, c = "r")
-    plt.plot(x, tensions_impl, label='Implicit Method', alpha = 0.8, c = "b")
-    plt.xlabel('Time (s)')
-    plt.ylabel('Maximum Tension (N)')
-    plt.title(f'Maximum Tension Comparison: fall factor {fall_factor_rk4}')
-    plt.legend()
-    plt.grid(True, alpha=0.3)
-    plt.show()
-
-def climber_jerk(rk4_file, implicit_file):
-    data_rk4 = np.load(rk4_file)
-    data_impl = np.load(implicit_file)
-    f_hist_rk4 = data_rk4['f_hist']
-    f_hist_impl = data_impl['f_hist']
-    fall_factor_rk4 = data_rk4['fall_factor']
-    masses_rk4 = data_rk4['masses']
-    masses_impl = data_impl['masses'] 
-    time = data_rk4['time']
-    t = f_hist_rk4.shape[0]
+    time = data_r1['time']
+    t = f_hist_rk1.shape[0]
 
     dt = time / (t - 1)
     x = np.arange(t) * dt  
     x_jerk = x[101:]
 
     a_hist = []
-    for f in f_hist_impl:
+    for f in f_hist_rk1:
         a = f[-1] / masses_impl[-1]
         a_hist.append(a)
 
@@ -196,7 +191,7 @@ def climber_jerk(rk4_file, implicit_file):
     jerk_mag = np.linalg.norm(jerk, axis=1)
 
     a_hist_rk4 = []
-    for f in f_hist_rk4:
+    for f in f_hist_rk2:
         a = f[-1] / masses_rk4[-1]
         a_hist_rk4.append(a)
 
@@ -205,11 +200,11 @@ def climber_jerk(rk4_file, implicit_file):
     jerk_mag_rk4 = np.linalg.norm(jerk, axis=1)
 
     plt.figure()
-    plt.plot(x_jerk,jerk_mag[100:],label='implicit')
-    plt.plot(x_jerk,jerk_mag_rk4[100:], label='RK4')
-    plt.title(f'Jerk experiensed by the climber, fall factor {fall_factor_rk4}')
+    plt.plot(x_jerk[:-10000],jerk_mag[100:-10000],label='implicit')
+    plt.plot(x_jerk[:-10000],jerk_mag_rk4[100:-10000], label='RK4')
+    plt.title(f'Jerk experienced by the climber, fall factor {fall_factor_rk4}')
     plt.xlabel('time (s)')
-    plt.ylabel('magnitude of jerk')
+    plt.ylabel('ms^-3')
     plt.legend()
     plt.show()
     
@@ -223,10 +218,10 @@ def main():
     rope5 = 'implicit_rope_simulation5.npz'
 
     sets = [rope1,rope2,rope3,rope4,rope5]
-    position(sets)
-    plot_force_comparison(sets)
+    #position(sets)
+    #plot_force_comparison(sets)
 
-    #plot_kinetic_energy_comparison(sets)
+    plot_kinetic_energy_comparison(sets)
     #tension(sets)
     #climber_jerk(sets)
 
